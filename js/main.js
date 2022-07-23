33339f48e9e6a5a3f569fb43bb0dc043cb7db633
Ramon Leonardo <ramon.c.leo@hotmail.com>
2022-07-23 18:46:12 -0300

    faz validação do formulário e exibe segunda tela

--- js/main.js
+++ js/main.js
@@ -3,21 +3,9 @@
 Primeira tela
 A primeira tela deve ter um formulário com os campos: nome, mensalidade, taxa de juros, tempo de contribuição e um botão simular.
 
-Ao clicar em simular, deve ser feito um POST na api http://api.mathjs.org/v4/.
-
-O body deverá ser um JSON com a seguinte estrutura: { "expr": "" }. Em expr vai a expressão matemática para calcular o juros compostos. Deverá ser usada a seguinte fórmula: [valor da mensalidade * (((1 + [taxa de juros]) ^ [tempo de contribuicao em meses] - 1) / [taxa de juros]).
-
-Exemplo:
-
-Se os parametros forem:
-
-Valor da mensalidade: R$ 20,00
-Taxa de juros: 0,517% ao mês
-Tempo de contribuição: 2 anos
-
 O body da request deverá ser:
-
 { "expr": "20 * (((1 + 0.00517) ^ 24 - 1) / 0.00517)" }
+
 Segunda tela
 A segunda tela deverá exibir um texto com as informaçoes dos campos de nome, mensalidade, tempo e o resultado da request.
 
@@ -40,45 +28,93 @@ form.onsubmit = function (e){
     let inputName = document.forms['form']['name']
     console.log(inputName.value)
 
+    if(!inputName.value) {
+        hasError = true
+        inputName.classList.add('error')
+        let span = inputName.nextSibling.nextSibling
+        span.innerText = 'Digite o nome corretamente'
+    } else {
+        inputName.classList.remove('error')
+        let span = inputName.nextSibling.nextSibling
+        span.innerText = ''
+    }
+
     let inputPayment = document.forms['form']['payment']
     console.log(inputPayment.value)
+
+    if (!inputPayment.value) {
+        hasError = true
+        inputPayment.classList.add('error')
+        let span = inputPayment.nextSibling.nextSibling
+        span.innerText = 'Digite um E-mail válido'
+    } else {
+        inputPayment.classList.remove('error')
+        let span = inputPayment.nextSibling.nextSibling
+        span.innerText = ''
+    }
 
     let inputTimeValue = document.forms['form']['timeValue']
     console.log(inputTimeValue.value)
 
+    if (!inputTimeValue.value) {
+        hasError = true
+        inputTimeValue.classList.add('error')
+        let span = inputTimeValue.nextSibling.nextSibling
+        span.innerText = 'Digite um E-mail válido'
+    } else {
+        inputTimeValue.classList.remove('error')
+        let span = inputTimeValue.nextSibling.nextSibling
+        span.innerText = ''
+    }
+
     let radio = document.forms['form']['radio']
     console.log(radio.value)
 
+    if (!radio.value ) {
+        hasError = true
+        console.log('erro')
+        radio[1].classList.add('error')
+        let span = radio[1].nextSibling.nextSibling
+        span.innerText = 'Selecione uma opção'
+    } else {
+        radio[1].classList.remove('error')
+        let span = radio[1].nextSibling.nextSibling
+        span.innerText = ''
+    }
+
     let btn = document.forms['form']['btn']
    
-    const expr =  `{"expr": "${inputPayment.value} * (((1 + 0.00517) ^ ${inputTimeValue.value} - 1) / 0.00517)"}`
-    console.log(expr)
-    
     const config = {
         headers: {
             "content-type": "application/json"
         },
         method: "POST",
-        body: expr,
+        body: `{"expr": "${inputPayment.value} * (((1 + 0.00517) ^ ${inputTimeValue.value} - 1) / 0.00517)"}`,
         
     }
     
     const toJson = (res) => {
-        return res.json()
-            
+        return res.json()     
     }
+    
     const data = (res) => {
-        console.log(res.result)
+        const result = res.result
+        console.log(result)
+        firstScreen.classList.remove('visible')
+        firstScreen.classList.add('hidden')
+        secondScreen.innerHTML = `<h1>Sua simulação retornou o seguinte valor:${result}</h1>`
     }
-
+    
     const errorHandling = error => console.log(error)
 
     fetch('http://api.mathjs.org/v4/', config).then(toJson).then(data).catch(errorHandling)
 
     
+        
+
     if (!hasError) {
 
-        form.submit()
+        form.submit()        
 
     }
 
